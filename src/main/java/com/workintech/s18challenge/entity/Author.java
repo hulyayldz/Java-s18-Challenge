e9bd4eabaff3e671700ec90a8ce50cebfde06b8a
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 18:57:13 +0300

    Create Author.java

---
+++ src/main/java/com/workintech/s18challenge/entity/Author.java
@@ -0,0 +1,25 @@
+package com.workintech.s18challenge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "author", schema = "public")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    
+}
