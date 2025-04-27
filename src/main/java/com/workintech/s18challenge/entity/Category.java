3376342e5ef77b6bcb2ff6e52a29799d3c24699c
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 18:57:48 +0300

    Create Category.java

---
+++ src/main/java/com/workintech/s18challenge/entity/Category.java
@@ -0,0 +1,22 @@
+package com.workintech.s18challenge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "category", schema = "public")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
