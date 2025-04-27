d757bf9b2228c9d1e6993fac19d12206c37f9b3d
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 18:57:31 +0300

    Create Book.java

---
+++ src/main/java/com/workintech/s18challenge/entity/Book.java
@@ -0,0 +1,31 @@
+package com.workintech.s18challenge.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.EnableMBeanExport;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "book", schema = "public")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+}
